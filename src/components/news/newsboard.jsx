9984bab6fcca5025e89a64a95e2f3e01e6a0984e
hasithaprageeth <haztha@gmail.com>
2024-03-24 15:06:07 +0000

    Fix loading error

--- src/components/news/newsboard.jsx
+++ src/components/news/newsboard.jsx
@@ -5,9 +5,8 @@ const Newsboard = ({ category }) => {
   const [articles, setArticles] = useState([]);
 
   useEffect(() => {
-    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${
-      import.meta.env.VITE_API_KEY
-    }`;
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=$bf72141aa1884b67947eaecc64ac543b
+    `;
     fetch(url)
       .then((response) => response.json())
       .then((data) => setArticles(data.articles));
