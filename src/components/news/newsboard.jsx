a186b0b2fdd25909ce45f85f464e8c29283eb63a
hasithaprageeth <haztha@gmail.com>
2024-03-24 15:09:36 +0000

    Revert the changes to loading error

--- src/components/news/newsboard.jsx
+++ src/components/news/newsboard.jsx
@@ -5,8 +5,9 @@ const Newsboard = ({ category }) => {
   const [articles, setArticles] = useState([]);
 
   useEffect(() => {
-    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=$bf72141aa1884b67947eaecc64ac543b
-    `;
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${
+      import.meta.env.VITE_API_KEY
+    }`;
     fetch(url)
       .then((response) => response.json())
       .then((data) => setArticles(data.articles));
